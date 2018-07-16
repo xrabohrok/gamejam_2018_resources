23a365e8b65a6f7741091ecb994c3a9f28369158
Mr.Black <westin007@gmail.com>
2018-07-15 22:17:48 -0500

    hey look choices now - Mr.black

--- Assets/code/InkDialoger/DialogueEngine.cs
+++ Assets/code/InkDialoger/DialogueEngine.cs
@@ -5,6 +5,7 @@ using Ink.Runtime;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Canvas))]
 public class DialogueEngine : MonoBehaviour
 {
     public TextAsset stuff;
@@ -15,11 +16,16 @@ public class DialogueEngine : MonoBehaviour
     private Dictionary<string, speaker> lookupPeeps;
     private Story storyPlayer;
     public Text textTarget;
+    public Transform ButtonPrefab;
+    public Transform buttonLocation;
+    public float buttonDisplacement;
 
     private string displayText;
     private string wholeText;
     private float progress;
     private bool readyToAdvance;
+    private bool choicesDrawn;
+    private List<GameObject> currentChoices;
 
     private bool advance;
 
@@ -31,6 +37,7 @@ public class DialogueEngine : MonoBehaviour
 	    readyToAdvance = false;
 	    advance = true;
 	    textTarget.text = "";
+        currentChoices = new List<GameObject>();
 	}
 
     // Update is called once per frame
@@ -46,15 +53,44 @@ public class DialogueEngine : MonoBehaviour
 
 	            advance = false;
 	            progress = 0;
-            }
+	            choicesDrawn = false;
+	        }
 
 
-	        if (readyToAdvance && Input.GetKeyUp(continueKey))
+	        if (readyToAdvance && storyPlayer.currentChoices?.Count > 0)
+	        {
+	            if (!choicesDrawn)
+	            {
+	                int i = 0;
+	                var basePos = buttonLocation.transform.position;
+
+	                clearChoices();
+	                foreach (var currentChoice in storyPlayer.currentChoices)
+	                {
+	                    var thing = Instantiate(ButtonPrefab, this.gameObject.transform);
+	                    var closureCount = i;
+	                    thing.gameObject.GetComponentInChildren<Text>().text = currentChoice.text;
+	                    thing.gameObject.GetComponent<Button>().onClick.AddListener(delegate {chosenChoiceAction(closureCount);});
+	                    thing.transform.position = new Vector3(basePos.x, basePos.y + i * buttonDisplacement, basePos.z);
+	                    currentChoices.Add(thing.gameObject);
+	                    i++;
+	                }
+
+	                choicesDrawn = true;
+	            }
+
+	        }
+            else if (readyToAdvance && Input.GetKeyUp(continueKey))
 	        {
 	            advance = true;
 	        }
 
-	        var charCount = wholeText.Length;
+	        if (storyPlayer.currentChoices?.Count == 0 && currentChoices.Count != 0)
+	        {
+                clearChoices();
+	        }
+
+            var charCount = wholeText.Length;
             
 	        var length = Mathf.CeilToInt( Mathf.Lerp(0, charCount - 1,  Mathf.Clamp(progress / timeToFinish, 0, 1)));
 //            Debug.Log($"charCount : {charCount}, substring : {length}, ticks: {progress}, readyToAdvance: {readyToAdvance}");
@@ -70,6 +106,25 @@ public class DialogueEngine : MonoBehaviour
 	            progress += Time.deltaTime;
             }
         }
+    }
+
+    private void clearChoices()
+    {
+        foreach (var currentChoice in currentChoices)
+        {
+            //TODO: let these things destroy themselves
+            Destroy(currentChoice);
+        }
+
+        currentChoices = new List<GameObject>();
+    }
+
+    void chosenChoiceAction(int choiceNum)
+    {
+        Debug.Log($"chose choice {choiceNum}");
+        storyPlayer.ChooseChoiceIndex(choiceNum);
+        advance = true;
+        choicesDrawn = false;
     }
 }
 
